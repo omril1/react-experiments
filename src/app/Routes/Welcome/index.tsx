3bb15df31c673500035dbd241a9ab6b610bc3252
omril1 <omri2593@gmail.com>
2018-12-29 16:39:14 +0200

    Added links from Welcome page

--- src/app/Routes/Welcome/index.tsx
+++ src/app/Routes/Welcome/index.tsx
@@ -5,6 +5,21 @@ export default function Welcome() {
   return (
     <div className={style.welcome}>
       <h1>Hello world</h1>
+
+      <ul>
+        <li>
+          <a href="/ParticleSwirl">ParticleSwirl</a>
+        </li>
+        <li>
+          <a href="/NeonHex">NeonHex</a>
+        </li>
+        <li>
+          <a href="/WebGLWater">WebGLWater</a>
+        </li>
+        <li>
+          <a href="/DonutSwirl">DonutSwirl</a>
+        </li>
+      </ul>
     </div>
   );
 }
