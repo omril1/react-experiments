5a3dfc8748e92b6c0f51f8e800687f9e89376eac
omril1 <omri2593@gmail.com>
2019-02-16 17:20:32 +0200

    Added a takeScreenshots script that uses puppeteer

---
+++ scripts/takeScreenshots.js
@@ -0,0 +1,50 @@
+//@ts-check
+const puppeteer = require('puppeteer');
+
+/* 
+  Taking a screen shot of all the screens with puppeteer
+*/
+
+const ROUTES = [
+  'ParticleSwirl',
+  'NeonHex',
+  'WebGLWater',
+  'DonutSwirl',
+  'RotatingParticleMesh',
+  'WebGLFluid',
+  'Shaders',
+  'RippleMousePlasma',
+  'Combined',
+  'CirclesPattern',
+];
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+
+  for (const route of ROUTES) {
+    try {
+      const page = await browser.newPage();
+      await page.goto('http://localhost:3000/' + route);
+      await sleep(200);
+      await page.screenshot({
+        path: `images/${route}.jpg`,
+        type: 'jpeg',
+        quality: 100,
+        fullPage: true,
+      });
+      await page.close();
+      console.log(`${route} done!`);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  await browser.close();
+})().catch(console.error);
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
